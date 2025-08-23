40fe4af8d06b4de3d8f0ce4251312e5e29ecf085
Bisonpasfute <thibault.cothenet@orange.fr>
2025-08-23 19:53:58 +0200

    Add tests

--- sim/druid/feral/feral_test.go
+++ sim/druid/feral/feral_test.go
@@ -31,9 +31,14 @@ func TestFeral(t *testing.T) {
 		Race:       proto.Race_RaceWorgen,
 		OtherRaces: []proto.Race{proto.Race_RaceTroll},
 		GearSet:    core.GetGearSet("../../../ui/druid/feral/gear_sets", "preraid"),
+		OtherGearSets: []core.GearSetCombo{
+			core.GetGearSet("../../../ui/druid/feral/gear_sets", "prehof"),
+			core.GetGearSet("../../../ui/druid/feral/gear_sets", "pretoes"),
+			core.GetGearSet("../../../ui/druid/feral/gear_sets", "p1"),
+		},
 
-		Talents:         StandardTalents,
-		Glyphs:          StandardGlyphs,
+		Talents: StandardTalents,
+		Glyphs:  StandardGlyphs,
 		OtherTalentSets: []core.TalentsCombo{
 			{Label: "SotF-HotW", Talents: "000101", Glyphs: StandardGlyphs},
 			{Label: "Incarn-NV", Talents: "000203", Glyphs: StandardGlyphs},
@@ -142,8 +147,8 @@ var PlayerOptionsMonoCatNoBleed = &proto.Player_FeralDruid{
 // }
 
 var FullConsumesSpec = &proto.ConsumesSpec{
-	FlaskId:     76084, // Flask of Spring Blossoms
-	FoodId:      74648, // Sea Mist Rice Noodles
-	PotId:       76089, // Virmen's Bite
-	PrepotId:    76089, // Virmen's Bite
+	FlaskId:  76084, // Flask of Spring Blossoms
+	FoodId:   74648, // Sea Mist Rice Noodles
+	PotId:    76089, // Virmen's Bite
+	PrepotId: 76089, // Virmen's Bite
 }
