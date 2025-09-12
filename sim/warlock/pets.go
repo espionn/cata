ce246a6301acca99b6e40b1088f23ac517bfd334
Adrian Klingen <adrian.klingen@deptagency.com>
2025-09-12 09:07:28 +0200

    Adjust succubus min energy and gcd

--- sim/warlock/pets.go
+++ sim/warlock/pets.go
@@ -170,7 +170,7 @@ func (warlock *Warlock) registerSuccubus() *WarlockPet {
 func (warlock *Warlock) registerSuccubusWithName(name string, enabledOnStart bool, isGuardian bool) *WarlockPet {
 	pet := warlock.RegisterPet(proto.WarlockOptions_Succubus, 3, 1.667, name, enabledOnStart, isGuardian)
 	pet.registerLashOfPainSpell()
-	pet.MinEnergy = 160
+	pet.MinEnergy = 60
 	return pet
 }
 
@@ -314,7 +314,7 @@ func (pet *WarlockPet) registerLashOfPainSpell() {
 		},
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{
-				GCD: core.GCDDefault,
+				GCD: time.Second,
 			},
 		},
 
