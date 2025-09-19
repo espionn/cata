387595e79fc68d2e684f2dc98fb4d9d5cf8c5463
NerdEgghead <nerdeggheadtbc@gmail.com>
2025-09-19 15:03:29 -0700

    Added off-hand slot to UI item swapping configuration
     On branch feral
     Changes to be committed:
    	modified:   ui/druid/feral/sim.ts

--- ui/druid/feral/sim.ts
+++ ui/druid/feral/sim.ts
@@ -108,7 +108,7 @@ const SPEC_CONFIG = registerSpecConfig(Spec.SpecFeralDruid, {
 			FeralInputs.CannotShredTarget,
 		],
 	},
-	itemSwapSlots: [ItemSlot.ItemSlotMainHand, ItemSlot.ItemSlotTrinket1, ItemSlot.ItemSlotTrinket2],
+	itemSwapSlots: [ItemSlot.ItemSlotMainHand, ItemSlot.ItemSlotOffHand, ItemSlot.ItemSlotTrinket1, ItemSlot.ItemSlotTrinket2],
 	encounterPicker: {
 		// Whether to include 'Execute Duration (%)' in the 'Encounter' section of the settings tab.
 		showExecuteProportion: true,
