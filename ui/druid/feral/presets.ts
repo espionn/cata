bd7f946706df6c712aa13f758a6f0dd4fa5ca0f8
NerdEgghead <nerdeggheadtbc@gmail.com>
2025-09-23 18:49:13 -0700

    Updated gear presets for Phase 2
     On branch feral
     Changes to be committed:
    	modified:   sim/druid/feral/TestFeral.results
    	modified:   ui/core/launched_sims.tsx
    	modified:   ui/druid/feral/gear_sets/p2.gear.json
    	modified:   ui/druid/feral/gear_sets/preraid.gear.json
    	modified:   ui/druid/feral/presets.ts
    	modified:   ui/druid/feral/sim.ts

--- ui/druid/feral/presets.ts
+++ ui/druid/feral/presets.ts
@@ -17,7 +17,7 @@ export const PRERAID_PRESET = PresetUtils.makePresetGear('Pre-Raid BiS', Preraid
 import P1Gear from './gear_sets/p1.gear.json';
 export const P1_PRESET = PresetUtils.makePresetGear('P1 BiS', P1Gear);
 import P2Gear from './gear_sets/p2.gear.json';
-export const P2_PRESET = PresetUtils.makePresetGear('P2', P2Gear);
+export const P2_PRESET = PresetUtils.makePresetGear('P2 BiS', P2Gear);
 import P3Gear from './gear_sets/p3.gear.json';
 export const P3_PRESET = PresetUtils.makePresetGear('P3', P3Gear);
 import P4Gear from './gear_sets/p4.gear.json';
