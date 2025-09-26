9771a9fbf97dd7e5641b877f7671c8070c023274
NerdEgghead <nerdeggheadtbc@gmail.com>
2025-09-26 13:24:07 -0700

    Swapped Guardian gear preset used in settings tests to P2 BiS
     On branch feral
     Changes to be committed:
    	modified:   sim/druid/guardian/TestGuardian.results
    	modified:   sim/druid/guardian/tank_test.go

--- sim/druid/guardian/tank_test.go
+++ sim/druid/guardian/tank_test.go
@@ -28,7 +28,7 @@ func TestGuardian(t *testing.T) {
 			Class: proto.Class_ClassDruid,
 			Race:  proto.Race_RaceWorgen,
 
-			GearSet: core.GetGearSet("../../../ui/druid/guardian/gear_sets", "preraid"),
+			GearSet: core.GetGearSet("../../../ui/druid/guardian/gear_sets", "p2"),
 
 			Talents: StandardTalents,
 			Glyphs:  StandardGlyphs,
