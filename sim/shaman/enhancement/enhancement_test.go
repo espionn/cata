e72c9dfed9c11a5d1087ff4258b2d95b1b939a52
Polynomix <Polynomix@protonmail.com>
2025-10-07 12:33:30 +0200

    enh p2 preset

--- sim/shaman/enhancement/enhancement_test.go
+++ sim/shaman/enhancement/enhancement_test.go
@@ -21,7 +21,7 @@ func TestEnhancement(t *testing.T) {
 			OtherRaces: []proto.Race{proto.Race_RaceDwarf, proto.Race_RaceTroll, proto.Race_RaceDraenei, proto.Race_RaceAlliancePandaren},
 
 			// The above line is the actual line for the ring but it is causing an error in the test
-			GearSet: core.GetGearSet("../../../ui/shaman/enhancement/gear_sets", "p1"),
+			GearSet: core.GetGearSet("../../../ui/shaman/enhancement/gear_sets", "simtest"),
 			Talents: TalentsASEB,
 			Glyphs:  StandardGlyphs,
 			OtherTalentSets: []core.TalentsCombo{
