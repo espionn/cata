67c208a79380775b6837db7a91965586c206e960
NerdEgghead <nerdeggheadtbc@gmail.com>
2025-10-09 09:37:00 -0700

    Move batch sim out of Experimental
     On branch feature/batch-sim
     Changes to be committed:
    	modified:   ui/core/components/gear_picker/item_list.tsx
    	modified:   ui/core/individual_sim_ui.tsx

--- ui/core/individual_sim_ui.tsx
+++ ui/core/individual_sim_ui.tsx
@@ -449,10 +449,10 @@ export abstract class IndividualSimUI<SpecType extends Spec> extends SimUI {
 
 	private addBulkTab(): BulkTab {
 		const bulkTab = new BulkTab(this.simTabContentsContainer, this);
-		bulkTab.navLink.hidden = !this.sim.getShowExperimental();
-		this.sim.showExperimentalChangeEmitter.on(() => {
-			bulkTab.navLink.hidden = !this.sim.getShowExperimental();
-		});
+		//bulkTab.navLink.hidden = !this.sim.getShowExperimental();
+		//this.sim.showExperimentalChangeEmitter.on(() => {
+		//	bulkTab.navLink.hidden = !this.sim.getShowExperimental();
+		//});
 		return bulkTab;
 	}
 
